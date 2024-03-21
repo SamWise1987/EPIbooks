b1e5c801d1b327e2899ec2bb231b27c0b2a385e2
SamWise1987 <samuelerea23@gmail.com>
2024-03-21 21:19:24 +0100

    Update Modal

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
-const container = document.querySelector('#cardContainer')
-const input = document.querySelector('#inputId')
-const button = document.querySelector('#searchBtn')
-let cart = []
-let books = []
+const container = document.querySelector('#cardContainer');
+const input = document.querySelector('#inputId');
+const button = document.querySelector('#searchBtn');
 
-function generateCard(book) {
-    const cardContainer = document.createElement('div');
-    cardContainer.setAttribute('class', 'col-12 col-md-4 col-lg-3');
-    cardContainer.setAttribute('id', book.asin);
-    cardContainer.innerHTML = /*html*/
-        `<div class="card" style="width: 18rem;">
-    <img src="${book.img}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${book.title}</h5>
-        <p class="card-text">${book.price} € </p>
-        <p class="card-text">${book.asin}</p>
-        <a href="#" class="btn btn-primary" id="btn-btn-btn">Aggiungi al carrello</a>
-    </div>
-    </div>`;
-
-    container.append(cardContainer);
-
-    //const addToCartButton = cardContainer.querySelector(`#btn-btn-btn`);
-    //addToCartButton.addEventListener("click", function () {
-    //    cardContainer.classList.add('added-to-cart');
-    //    cart.push(book);
-
-    // localStorage.setItem('cart', JSON.stringify(cart));
-
-    const addToCartButton = cardContainer.querySelector('.btn-primary');
-    addToCartButton.id = `add-to-cart-${book.asin}`;
-    addToCartButton.addEventListener("click", function () {
-        if (!cart.some(item => item.asin === book.asin)) {
-            cart.push(book);
-            
-            addToCartButton.textContent = "Aggiunto";
-            addToCartButton.disabled = true;
-        }
-
-        const card = cardContainer.querySelector('.card-body');
-        card.classList.add('selected');
-    });
-}
-
-
+let books = [];
+const savedCart = JSON.parse(localStorage.getItem("cart"));
+let cart = savedCart ? savedCart : [];
 
 async function getBooks() {
     try {
@@ -55,9 +16,63 @@ async function getBooks() {
     }
 }
 
+function generateCard(book) {
+    const cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class', 'col-12 col-md-4 col-lg-3 pb-2');
+    cardContainer.setAttribute('id', book.asin);
+    cardContainer.innerHTML = /*html*/
+        `    <div class="card" style="width: 18rem;">
+            <img src="${book.img}" class="card-img-top book-cover" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-truncate">${book.title}</h5>
+                <p class="card-text">${book.price} € </p>
+                <p class="card-text">${book.asin}</p>
+                
+                <button type="button" class="btn btn-primary" id="btn-${book.asin}">Aggiungi al carrello</button>
+            </div>
+        </div>`;
+
+    container.append(cardContainer);
+
+    const addToCartButton = cardContainer.querySelector(`#btn-${book.asin}`);
+    addToCartButton.addEventListener("click", function () {
+        cardContainer.classList.add('added-to-cart');
+        cart.push(book);
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+        // Qui aggiorno il modale
+        //updateCart(book);
+    });
+}
+
+// popolo il modale
+//function updateCart(book) {
+//    const cartList = document.querySelector('#cartList');
+//    const listItems = document.createElement('li');
+//    listItems.textContent = `${book.title} - ${book.price} €`;
+
+//    cartList.appendChild(listItems);
+//}
+
+const cartModal = document.querySelector('.btn-modal');
+const updateCartMarco = () => {
+    if (savedCart) {
+        savedCart.forEach((cartItem) => {
+            const div = document.createElement('div');
+            div.innerHTML = `${cartItem.title} - ${cartItem.price} €`;
+            cartModal.appendChild(div);
+        });
+    } else {
+        const paragraph = document.createElement('p');
+        paragraph.innerText = "Non ci sono elementi nel carrello";
+        cartModal.appendChild(paragraph);
+    }
+};
+
 getBooks().then(books => {
-    books.map((book) => generateCard(book))
-})
+    books.map((book) => generateCard(book));
+});
 
 async function filterBooks(query) {
     const books = await getBooks();
@@ -70,26 +85,24 @@ async function filterBooks(query) {
 }
 
 function searchBooks() {
-    const searchTerms = document.querySelector("#inputId").value
+    const searchTerms = document.querySelector("#inputId").value;
 
-    container.innerHTML = ""
+    container.innerHTML = "";
     for (let book of books) {
         if (book.title.toLowerCase().includes(searchTerms.toLowerCase())) {
-            generateCard(book)
+            generateCard(book);
         }
     }
 }
-
-
 
 input.addEventListener("change", async () => {
     console.log("changed");
     if (input.value === "") {
         getBooks().then(books => {
-            books.map((book) => generateCard(book))
-        })
+            books.map((book) => generateCard(book));
+        });
     }
-})
+});
 
 button.addEventListener("click", async () => {
     container.innerHTML = "";
@@ -98,9 +111,3 @@ button.addEventListener("click", async () => {
         filteredBooks.forEach(book => generateCard(book));
     });
 });
-
-
-
-// il rallentamento è perchPé uso una async all'interno di un eventlistner e
-//creare delle funzioni dedicat agli eventlistner
-// dovrei astrarre i then in altr funzioni fuori dagli eventlistner e lancerà roba sincrona 
